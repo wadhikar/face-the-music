78119709b7937cfc1b81c1d88562a5cecfc1ec34
Wilson Adhikari <wilsonadhikari@gmail.com>
2020-10-19 19:04:10 -0700

    Add environment variables

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const queryString = require('querystring');
+const dotenv = require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 
