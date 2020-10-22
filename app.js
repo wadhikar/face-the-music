e1ccd1de3b07db058f895d1e7268f25d5a0ac1a7
Wilson Adhikari <wilsonadhikari@gmail.com>
2020-10-21 19:54:13 -0700

    Adjust ignoreFavicon placement

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(ignoreFavicon);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(ignoreFavicon);
 
 app.use('/', indexRouter);
 
