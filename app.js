f646f23da8c7377b88954326f949699d1584e6f6
Wilson Adhikari <wadhikar@users.noreply.github.com>
2021-02-28 21:31:52 -0800

    Connect to React front end. Some clean up.

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../frontend/ftm-react/build')));
 
 app.use('/', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -183,33 +183,13 @@ const apiUpload = upload.single('userImage');
 
 router.post('/api/upload', cors(), apiUpload, asyncHandler(async(req, res, next) => {
   
-  console.log("Received request from FileInput");
-
-  console.log("Headers: " + req.headers[0] + '\n');
-  console.log("Params: " + req.params[0] + '\n');
-  console.log("Body: " + req.body[0] + '\n');
-  console.log("File: " + req.file + '\n');
-  // console.log("Files: " + req.files[0] + '\n');
-
-  // console.log("Is buffer populated? " + req.files);
-  // console.log("Is buffer populated? ");
-  // console.log(req.files);
-  
-  for (const item in req.file) {
-    // console.log("key: " + key);
-    console.log(item);
-    // console.log(value);
-  }
+  console.log("Received request from React");
 
   // Can pass buffer to Face API
   const image = req.file.buffer;
   let emotion = '';
   let results = [];
   let playlistId = '';
-
-  console.log("1st msg: Received request from FileInput");
-
-  console.log("res._headers >>>>>>>" + JSON.stringify(res._headers));
 
   console.log("Image Buffer: ", image);
 
@@ -222,13 +202,9 @@ router.post('/api/upload', cors(), apiUpload, asyncHandler(async(req, res, next)
       console.log(result[0].faceAttributes.emotion);
       emotion = getHighestEmotion(result[0].faceAttributes.emotion);
       console.log("The strongest emotion is: ", emotion);
-      // next();
+      // next(); // Clean up
     });
   
-  console.log("res._headers >>>>>>>" + JSON.stringify(res._headers));
-  
-  // Currently stops here, probably because tries to execute but emotion value isn't set in promise
-  // Might also have issues with multiple requests happening since SpotifyAPI probably does so under the hood
   console.log("Getting playlist after Face API: ", emotion);
 
   await spotifyAPI.clientCredentialsGrant().then(
@@ -280,12 +256,7 @@ router.post('/api/upload', cors(), apiUpload, asyncHandler(async(req, res, next)
     results = results.flat();
 
     playlistId = spotifyTrim.getRandomPlaylistId(results);
-    // let spotifyExternalURL = spotifyTrim.buildOpenSpotifyURL(playlistId);
-    
-    // (async () => {
-    //   await open(spotifyExternalURL);
-    // })();
-    
+
   }, function(err) {
     console.log("Something went wrong!", err);
     next(err);
@@ -299,134 +270,5 @@ router.post('/api/upload', cors(), apiUpload, asyncHandler(async(req, res, next)
   });
 
 }));
-
-// /* 
-// Endpoint for React App
-// Receive image data in request.
-// Get playlist and return URI and external URL
-// */
-// // const apiUpload = upload.fields([
-// //   { name: 'image' }
-// // ]);
-// const apiUpload = upload.single('userImage');
-
-// router.post('/api/upload', cors(), apiUpload, function(req, res, next) {
-  
-//   console.log("Received request from FileInput");
-
-//   console.log("Headers: " + req.headers[0] + '\n');
-//   console.log("Params: " + req.params[0] + '\n');
-//   console.log("Body: " + req.body[0] + '\n');
-//   console.log("File: " + req.file + '\n');
-//   // console.log("Files: " + req.files[0] + '\n');
-
-//   // console.log("Is buffer populated? " + req.files);
-//   // console.log("Is buffer populated? ");
-//   // console.log(req.files);
-  
-//   for (const item in req.file) {
-//     // console.log("key: " + key);
-//     console.log(item);
-//     // console.log(value);
-//   }
-
-//   // Can pass buffer to Face API
-//   const image = req.file.buffer;
-//   let emotion = '';
-//   let results = [];
-//   let playlistId = '';
-
-//   console.log("1st msg: Received request from FileInput");
-
-//   // console.log("Image Buffer: ", image);
-//   (async () => {
-//     await faceClient.face
-//       .detectWithStream(image, faceDetectFromStreamOptionalParams)
-//       .then(result => {
-//         console.log("The result is: ");
-//         console.log(result);
-//         console.log("The emotions are: ");
-//         console.log(result[0].faceAttributes.emotion);
-//         emotion = getHighestEmotion(result[0].faceAttributes.emotion);
-//         console.log("The strongest emotion is: ", emotion);
-//         next();
-//       });
-    
-//     // Currently stops here, probably because tries to execute but emotion value isn't set in promise
-//     // Might also have issues with multiple requests happening since SpotifyAPI probably does so under the hood
-//     console.log("Getting playlist after Face API: ", emotion);
-
-//     await spotifyAPI.clientCredentialsGrant().then(
-//       function(data) {
-//         console.log("Received token!");
-        
-//         // Set access token for use in future calls
-//         spotifyAPI.setAccessToken(data.body['access_token']);
-//       },
-//       function(err) {
-//         console.log(
-//           'Something went wrong when retrieving an access token',
-//           err.message
-//         )
-//         next(err);
-//       }
-//     )
-    
-//     await spotifyAPI.getPlaylistsForCategory('mood', {
-//       country: 'CA',
-//       limit : 50,
-//       offset : 0
-//     })
-//     .then(function(data) {
-//       console.log("Received results!");
-//       let emotionPlaylists = spotifyTrim.getPlaylistsFromFaceEmotion(emotion, data.body)
-//       results.push(emotionPlaylists);
-      
-//     }, function(err) {
-//       console.log("Something went wrong!", err);
-//       next(err);
-//     });
-//     await spotifyAPI.getPlaylistsForCategory('mood', {
-//       country: 'CA',
-//       limit : 50,
-//       offset : 50
-//     })
-//     .then(function(data) {
-//       console.log("Received results!");
-//       let emotionPlaylists = spotifyTrim.getPlaylistsFromFaceEmotion(emotion, data.body)
-//       results.push(emotionPlaylists);
-
-//       // Ensure our array is flat
-//       results = results.flat();
-
-//       playlistId = spotifyTrim.getRandomPlaylistId(results);
-//       console.log(playlistId);
-//       // let spotifyExternalURL = spotifyTrim.buildOpenSpotifyURL(playlistId);
-      
-//       // (async () => {
-//       //   await open(spotifyExternalURL);
-//       // })();
-
-//       // return res.status().json({
-//       //   uri: 'spotify:playlist:' + playlistId,
-//       //   embedded: 'https://open.spotify.com/embed/playlist/' + playlistId,
-//       // });
-      
-//     }, function(err) {
-//       console.log("Something went wrong!", err);
-//       next(err);
-//     });
-
-//     res.json({
-//       uri: 'spotify:playlist:' + playlistId,
-//       embedded: 'https://open.spotify.com/embed/playlist/' + playlistId,
-//     });
-//   })();
-
-//   // File is being sent
-//   console.log("finished async function");
-
-//   // res.status(200).send('Finished operations')
-// });
 
 module.exports = router;
